f410678ca20554082d7d6d1d42f2c224b281bd18
Lê Trần Quốc Bảo <123377752+quocbao2772004@users.noreply.github.com>
2024-08-02 16:01:46 +0700

    Update solve.py

--- solve.py
+++ solve.py
@@ -1,18 +1,18 @@
 import ultralytics, os, cv2 
 from ultralytics import YOLO
 from put_text import put_text_with_pillow
-from paddle_ocrr import read_text_using_paddleocr, return_result
+from paddle_ocrr import read_text_using_paddleocr, read_text_using_paddleocr_part_2
 from tesseractt import preprocess_image, extract_text_from_image
 from processing import tao_nen, show_lisence_plate
-from check import check_plate
+
 from ultralytics.utils.plotting import Annotator, colors
 import matplotlib.pyplot as plt
 
 font_size = 18
 color = (178 ,34, 34)
 color_rec=(178 ,34, 34)
-font_path = r"C:\Users\PC\Downloads\font ttf tiếng việt\2BYTE\VBODONP.TTF"
-model = YOLO(r'F:\project\plate\model\plate.pt')
+font_path = r"/home/quoc-bao/Downloads/Bo-Font-Full/Bo-Font-tieng-Trung-Quoc/Bộ Font tiếng Trung Quốc/14-bo-font-tieng-trung-dac-biet- chinesecomvn/fzstk.ttf"
+model = YOLO(r'/home/quoc-bao/code/Lisence_plate/license_plate/model/plate2.pt')
 
 def crop_img_and_show_plate(im0):
             results = model.predict(im0)
@@ -22,18 +22,27 @@ def crop_img_and_show_plate(im0):
                 x1, y1, x2, y2 = map(int, box)
                 cropped_img = im0[y1:y2, x1:x2]
                 text=read_text_using_paddleocr(cropped_img)
+                if text == "":
+                    text = read_text_using_paddleocr_part_2(im0)
+                if text == "":
+                    text = "Error"
                 return 1,text,x1,y1,x2,y2
             return 0,None, None, None, None, None
         
 def image():
-    for image in os.listdir(r'F:\project\plate\makedataset\GreenParking\GreenParking'):
-        path=os.path.join(r'F:\project\plate\makedataset\GreenParking\GreenParking', image)
+    data1='/home/quoc-bao/code/Lisence_plate/license_plate/makedataset/GreenParking'
+    data2='/home/quoc-bao/code/easy_ocr/CarTGMT'
+    for image in os.listdir(data2):
+        print(image)
+        path=os.path.join(data2, image)
         im0=cv2.imread(path)
         status, text, x1, y1, x2, y2 = crop_img_and_show_plate(im0)
         if status:
+            
             print(text)
             img=cv2.imread(path)
-            position_number = (480,180)
+            height, width = im0.shape[:2]
+            position_number = (width+2, height//2 + 20)
 
             cv2.rectangle(img, (x1, y1), (x2, y2), color_rec, 2)
             img=show_lisence_plate(img, img[y1:y2,x1:x2])
