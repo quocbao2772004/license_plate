37d5772238cf39c80cdd90ebe1c5bebc5ae1f356
Lê Trần Quốc Bảo <123377752+quocbao2772004@users.noreply.github.com>
2024-08-02 17:33:10 +0700

    Update solve.py

--- solve.py
+++ solve.py
@@ -3,8 +3,8 @@ from ultralytics import YOLO
 from put_text import put_text_with_pillow
 from paddle_ocrr import read_text_using_paddleocr, read_text_using_paddleocr_part_2
 from tesseractt import preprocess_image, extract_text_from_image
-from processing import tao_nen, show_lisence_plate
-
+from processing import show_lisence_plate
+import time
 from ultralytics.utils.plotting import Annotator, colors
 import matplotlib.pyplot as plt
 
@@ -14,6 +14,27 @@ color_rec=(178 ,34, 34)
 font_path = r"/home/quoc-bao/Downloads/Bo-Font-Full/Bo-Font-tieng-Trung-Quoc/Bộ Font tiếng Trung Quốc/14-bo-font-tieng-trung-dac-biet- chinesecomvn/fzstk.ttf"
 model = YOLO(r'/home/quoc-bao/code/Lisence_plate/license_plate/model/plate2.pt')
 
+def create_paddingg(original_image):
+    if original_image is None:
+        print("Error: Could not read the image.")
+        return None
+    else:
+        
+        height, width, channels = original_image.shape
+        top_padding = 10
+        bottom_padding = 10
+        left_padding = 10
+        right_padding = 10
+        
+        padded_image = cv2.copyMakeBorder(
+            original_image,
+            top_padding, bottom_padding, left_padding, right_padding,
+            borderType=cv2.BORDER_CONSTANT,
+            value=[0, 0, 0]  
+        )
+        
+        return padded_image
+    
 def crop_img_and_show_plate(im0):
             results = model.predict(im0)
             boxes = results[0].boxes.xyxy.cpu().numpy()
@@ -21,6 +42,7 @@ def crop_img_and_show_plate(im0):
             for i, box in enumerate(boxes):
                 x1, y1, x2, y2 = map(int, box)
                 cropped_img = im0[y1:y2, x1:x2]
+                cropped_img = create_paddingg(cropped_img)
                 text=read_text_using_paddleocr(cropped_img)
                 if text == "":
                     text = read_text_using_paddleocr_part_2(im0)
@@ -34,6 +56,7 @@ def image():
     data2='/home/quoc-bao/code/easy_ocr/CarTGMT'
     for image in os.listdir(data2):
         print(image)
+        start_time=time.time()
         path=os.path.join(data2, image)
         im0=cv2.imread(path)
         status, text, x1, y1, x2, y2 = crop_img_and_show_plate(im0)
@@ -46,44 +69,19 @@ def image():
 
             cv2.rectangle(img, (x1, y1), (x2, y2), color_rec, 2)
             img=show_lisence_plate(img, img[y1:y2,x1:x2])
+            end_time=time.time()
+            total_time=end_time-start_time
             img_putted_text = put_text_with_pillow(img, text, position_number, font_path, font_size, color)
-            
+            img_putted_text = put_text_with_pillow(img_putted_text, str(total_time), (width+2,height//1.1), font_path, font_size, color)
+            # print(total_time)
             cv2.imshow("Show", img_putted_text)
             cv2.waitKey(0)
+            
         else:
             cv2.imshow("Show", im0)
         
-def video():
-
-    video_path = r"F:\project\plate\makedataset\bike_counter_10min.mp4"
-    cap = cv2.VideoCapture(video_path)
-    while cap.isOpened():
-
-        success, frame = cap.read()
-        if success:
-            frame = cv2.resize(frame, (640, 480))
-            results = model.track(frame, persist=True)
-            annotated_frame = results[0].plot()
-            cv2.imshow("show", frame)
-
-            if results[0].boxes:
-                for box in results[0].boxes.xyxy:
-                    x1, y1, x2, y2 = map(int, box[:4])
-                    cropped_img = frame[y1:y2, x1:x2]
-                    
-                    text = return_result(cropped_img)
-                    print(text)
-                    # cv2.imshow("Cropped Image", cropped_img)
-                    # cv2.imwrite(f'cropped_{x1}_{y1}.png', cropped_img)
-
-            if cv2.waitKey(10) & 0xFF == ord("q"):
-                break
-        else:
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-       
+        
+            
 def main():
     image()
     
